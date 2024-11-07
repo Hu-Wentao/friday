f19423356812793b7cc756b27b2cbe339c08cbbc
胡文涛 <35894003+Hu-Wentao@users.noreply.github.com>
2024-11-07 23:06:32 +0800

    refactor state

--- pages/stream_chat.py
+++ pages/stream_chat.py
@@ -3,9 +3,16 @@ from openai import OpenAI
 
 from src.shared_state import se_llm
 
+
 # === state
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+def s_messages(messages=None, append: dict = None):
+    if 'messages' not in st.session_state:
+        st.session_state['messages'] = []
+    if messages is not None:
+        st.session_state["messages"] = messages
+    if append is not None:
+        st.session_state["messages"].append(append)
+    return st.session_state["messages"]
 
 
 def s_regen_flag(regen=None, consume=False):  # 是否重新生成回答
@@ -41,6 +48,24 @@ def get_llm() -> OpenAI:
     return se_llm(st.session_state["llm_api"])
 
 
+def get_llm_rsp(llm=None, model=None, msg_ls=None, stream=True):
+    if llm is None:
+        llm = get_llm()
+    if model is None:
+        model = st.session_state["openai_model"]
+    if msg_ls is None:
+        msg_ls = [{
+            "role": m["role"],
+            "content": m["content"],
+            # "name": m.get("name")
+        } for m in st.session_state.messages]
+    return llm.chat.completions.create(
+        model=model,
+        messages=msg_ls,
+        stream=stream,
+    )
+
+
 def get_avatar_by_role(role: str):
     if role == "user":
         return "👨‍💻"
@@ -57,58 +82,47 @@ def on_chat_submit():
 
 
 # ==== GUI
-st.set_page_config(layout='wide')
+def main():
+    st.set_page_config(layout='wide')
 
-st.title("Friday Chat")
+    st.title("Friday Chat")
 
-with st.sidebar:
-    st.selectbox("LLM", ["friday_0", "friday_1"], key="llm_api", index=0)
-    # gpt-4o-mini               (0.0000135, 0.000054) # Azure(0.0000675, 0.00027)
-    # gpt-4o                    (0.000225 , 0.000900) # Azure(0.0011250, 0.004500)
-    # claude-3-haiku-20240307   (0.000198, 0.00099)   #
-    # claude-3-5-sonnet-20241022(0.002376, 0.01188)
-    # claude-3-opus-20240229    (0.011880, 0.05940)
-    st.selectbox(key="openai_model", index=0, label="Model", options=[
-        "gpt-4o-mini", "gpt-4o",
-        "claude-3-haiku-20240307", "claude-3-5-sonnet-20241022", "claude-3-opus-20240229", ], )
+    with st.sidebar:
+        st.selectbox("LLM", ["friday_0", "friday_1"], key="llm_api", index=0)
+        # gpt-4o-mini               (0.0000135, 0.000054) # Azure(0.0000675, 0.00027)
+        # gpt-4o                    (0.000225 , 0.000900) # Azure(0.0011250, 0.004500)
+        # claude-3-haiku-20240307   (0.000198, 0.00099)   #
+        # claude-3-5-sonnet-20241022(0.002376, 0.01188)
+        # claude-3-opus-20240229    (0.011880, 0.05940)
+        st.selectbox(key="openai_model", index=0, label="Model", options=[
+            "gpt-4o-mini", "gpt-4o",
+            "claude-3-haiku-20240307", "claude-3-5-sonnet-20241022", "claude-3-opus-20240229", ], )
 
-for i, message in enumerate(st.session_state.messages):
-    with st.chat_message(message["role"], avatar=get_avatar_by_role(message["role"])):
-        col = st.columns([14, 1])
-        col[0].markdown(message["content"])
-        col[1].selectbox(key=f'chat_msg_menu#{i}', label='Menu', options=['', '❌', '📝', '🔄'],
-                         label_visibility='collapsed', on_change=on_chat_msg_menu_change)
+    for i, message in enumerate(st.session_state.messages):
+        with st.chat_message(message["role"], avatar=get_avatar_by_role(message["role"])):
+            col = st.columns([14, 1])
+            col[0].markdown(message["content"])
+            col[1].selectbox(key=f'chat_msg_menu#{i}', label='Menu', options=['', '❌', '📝', '🔄'],
+                             label_visibility='collapsed', on_change=on_chat_msg_menu_change)
 
-if prompt := st.chat_input("What is up?", key="chat_input", on_submit=on_chat_submit):
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    # Display user message in chat message container
-    with st.chat_message("user", avatar=get_avatar_by_role('user')):
-        st.markdown(prompt)
+    # 用户输入
+    if prompt := st.chat_input("What is up?", key="chat_input", on_submit=on_chat_submit):
+        s_messages(append={"role": "user", "content": prompt})
+        with st.chat_message("user", avatar=get_avatar_by_role('user')):
+            st.markdown(prompt)
 
-    with st.chat_message("assistant", avatar=get_avatar_by_role('assistant')):
-        stream = get_llm().chat.completions.create(
-            model=st.session_state["openai_model"],
-            messages=[{
-                "role": m["role"],
-                "content": m["content"],
-                # "name": m.get("name")
-            } for m in st.session_state.messages],
-            stream=True,
-        )
-        response = st.write_stream(stream)
-    st.session_state.messages.append({"role": "assistant", "content": response})
+        with st.chat_message("assistant", avatar=get_avatar_by_role('assistant')):
+            response = st.write_stream(get_llm_rsp())
+        s_messages(append={"role": "assistant", "content": response})
 
-if s_regen_flag(consume=True):
-    with st.chat_message("assistant", avatar=get_avatar_by_role('assistant')):
-        stream = get_llm().chat.completions.create(
-            model=st.session_state["openai_model"],
-            messages=[{
-                "role": m["role"],
-                "content": m["content"],
-                # "name": m.get("name")
-            } for m in st.session_state.messages],
-            stream=True,
-        )
-        response = st.write_stream(stream)
-    st.session_state.messages.append({"role": "assistant", "content": response})
+    # 重新生成
+    if s_regen_flag(consume=True):
+        with st.chat_message("assistant", avatar=get_avatar_by_role('assistant')):
+            response = st.write_stream(get_llm_rsp())
+        s_messages(append={"role": "assistant", "content": response})
+
+
+if __name__ == '__main__':
+    s_messages()
+    # ===
+    main()
